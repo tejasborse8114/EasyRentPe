f2493dbf7226189ac947a9d8891a188389d1b4ca
akshay borse <akshayborse2016@gmail.com>
2026-09-21 20:52:22 +0530

    Website ready with simple style

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'easyrentpe',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      cwd: __dirname,
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
